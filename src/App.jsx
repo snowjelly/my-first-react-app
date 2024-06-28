63a26cd7301a80ba970ea7fcb2696e0aa3efa73a
snowinjelly <broadwellconnor@gmail.com>
2024-06-28 14:16:43 -0500

    guard statements

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,18 @@ const AnimalList = (animals) => {
   }
 
   function List(props) {
+    if (!props.animals) {
+      return <div>Loading...</div>;
+    }
+
+    if (props.animals.length === 0) {
+      return <div>There are no animals in the list!</div>;
+    }
+
     return (
       <ul>
         {props.animals.map((animal) => {
-          return (
-            animal.startsWith("L") && <ListItem key={animal} animal={animal} />
-          );
+          return <ListItem key={animal} animal={animal} />;
         })}
       </ul>
     );
